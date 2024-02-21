d6a6f57e76406e672932e18740448fddf31f3482
kola kolade <kolakolade@gmail.com>
2024-02-21 17:02:12 +0100

    Add simple routing

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-const http = require('https');
+const http = require('http');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'content-Type': 'text/plain' });
-  res.end('Hello World');
+  const path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
+  switch (path) {
+    case '':
+      res.writeHead(200, {
+        'content-Type': 'text/plain',
+      });
+      res.end('Homepage');
+      break;
+    case '/about':
+      res.writeHead(200, {
+        'content-Type': 'text/plain',
+      });
+      res.end('About page');
+      break;
+    default:
+      res.writeHead(404, {
+        'content-Type': 'text/plain',
+      });
+      res.end('404: Page not found');
+      break;
+  }
 });
 
 server.listen(port, () => {
   console.log(`Server listening on port: ${port}...`);
-  console.log(`Press ctrl+c to terminate server`);
+  console.log(`Press ctrl+c to terminate the server`);
 });
